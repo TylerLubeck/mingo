25590c2c8c0d21a4c8e97b7d280e7f210439a2e8
Tyler Lubeck <tyler@tylerlubeck.com>
2013-10-03 16:39:52 -0400

    Show table on homepage

--- static/js/loadData.js
+++ static/js/loadData.js
@@ -3,7 +3,43 @@ console.log('loaded file');
 function loadTable() {
 	console.log('helloooooooo')
 	$.get('/possibilities.json', function(data){
-		console.log(data);
+		data = JSON.parse(data);
+		//console.log(data.length);
+		//console.log(data);
+		var table = $('<table></table>');
+		var tr = $('<tr></tr>');
+		var xPos = 1;
+		var yPos = 1;
+		var width = 5;
+		var height = 5;
+
+		for (i in data){
+
+			if(xPos > width) {
+				$(table).append(tr);
+				tr = $('<tr></tr>');
+				xPos = 0;
+				yPos++;
+			}
+			if(yPos > height){
+				break;
+			}
+
+			var td = $('<td></td>');
+			//console.log(data[i].square)
+			$(td).text(data[i].square);
+			$(tr).append(td);
+
+
+
+
+			xPos++;
+		}
+		console.log(table);
+
+
+		$('#content').append(table);
+		console.log($('#content').html());
 	})
 }
 
