05504a80f79e9b0b3f4564bac6649423076ded90
Tyler Lubeck <tyler@tylerlubeck.com>
2014-02-02 12:23:42 -0500

    Remove... questionable items

--- static/js/possibilities.js
+++ static/js/possibilities.js
@@ -5,7 +5,7 @@ function loadPossibilities() {
 		var table = $('<table></table>');
 		var head = $('<thead></thead>');
 		var tr = $('<tr></tr>');
-		tr.append($('<td></td><td>Phrase</td><td>Drinking Behavior</td><td>Date Added</td>'));
+		tr.append($('<td></td><td>Phrase</td><td>Date Added</td>'));
 		$(head).append(tr);
 		$(table).append(head);
 		var body = $('<tbody></tbody>');
@@ -13,7 +13,7 @@ function loadPossibilities() {
 		var num = 1;
 		data.forEach(function(poss){
 			tr = $('<tr></tr>');
-			var td = $('<td>' + num + '</td><td>' + poss.square + '</td><td>' + poss.numDrinks + '</td><td>' + poss.dateAdded + '</td>');
+			var td = $('<td>' + num + '</td><td>' + poss.square + '</td><td>' + poss.dateAdded + '</td>');
 			$(tr).append(td);
 			$(body).append(tr);		
 			num++;
@@ -21,7 +21,7 @@ function loadPossibilities() {
 
 		var form = $('<form id="addQuote" action="/AddSquare"></form>');
 		form.append($('<input name="square" type="text" id="square" placeholder="Quote"></br>'));
-		form.append($('<input name = "numDrinks" type="text" id="numDrinks" placeholder="Number of Drinks"></br>'));
+		form.append($('<input class="hidden" value="1" name = "numDrinks" type="text" id="numDrinks" placeholder="Number of Drinks"></br>'));
 		var passwordField = $('<input name="password" type="password" id="password" placeholder="Password"></br>')
 		form.append(passwordField);
 		var button = $('<input name="submit" type="button" id="submitButton" value="Submit">') 
